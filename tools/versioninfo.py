b21359c6d6403451948f63ea39e546abfecffe34
FB <frederic.balmer@gmail.com>
2014-02-13 12:00:01 +0100

    Added versioninfo.py and edited .gitignore.

---
+++ tools/versioninfo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import platform
+import numpy
+import scipy
+import matplotlib
+
+print "Architecture: %s " % str(platform.architecture())
+print "System: %s " % platform.system()
+print "Release: %s " % platform.release()
+print "Version: %s " % platform.version()
+print
+print "Python: %s " % platform.python_version()
+print "NumPy: %s " % numpy.__version__
+print "SciPy: %s " % scipy.__version__
+print "Matplitlib: %s" % matplotlib.__version__
